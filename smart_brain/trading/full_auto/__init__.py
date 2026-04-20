704cd65229ae8c1b9708d07c87b30283ee013c87
shl888 <shl888shl888@outlook.com>
2026-04-20 21:57:50 +0800

    Add files via upload

--- smart_brain/trading/full_auto/__init__.py
+++ smart_brain/trading/full_auto/__init__.py
@@ -1,15 +1,20 @@
-# trading/full_auto/__init__.py
 """
 全自动交易模块
 
-包含三个工人：
-- auto_open: 侦察兵，负责检测开仓条件，生成开仓指令
-- auto_sltp: 止损止盈工人，负责设置止损止盈
-- auto_close: 清仓工人，负责持续监控并触发清仓
+包含两套策略：
+- funding: 资金费套利
+- spread: 价差套利
 """
 
-from .auto_open import Scout as AutoOpen
-from .auto_sltp import FullAutoSlTpWorker as AutoSlTp
-from .auto_close import FullAutoCloser as AutoClose
+from .funding.open import FundingOpen
+from .funding.close import FundingClose
+from .funding.sltp import FundingSlTp
 
-__all__ = ['AutoOpen', 'AutoSlTp', 'AutoClose']
+from .spread.open import SpreadOpen
+from .spread.close import SpreadClose
+from .spread.sltp import SpreadSlTp
+
+__all__ = [
+    'FundingOpen', 'FundingClose', 'FundingSlTp',
+    'SpreadOpen', 'SpreadClose', 'SpreadSlTp',
+]
